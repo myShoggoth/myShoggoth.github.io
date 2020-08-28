199d4323421754427b58bdf6cc896909addbac95
Andrew Boardman <andrew@myshoggoth.com>
2020-08-28 11:36:23 -0700

    Add `dotFileRoute`
    So we can have a `gitignore` file that becomes `.gitignore` in the site.

--- site.hs
+++ site.hs
@@ -75,59 +75,20 @@ main = hakyll $ do
         route idRoute
         compile copyFileCompiler
 
-    match ".gitignore" $ do
-        route idRoute
+    match "gitignore" $ do
+        route dotFileRoute
         compile copyFileCompiler
         
     match "templates/**" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
+dotFileRoute :: Routes
+dotFileRoute = customRoute $
+    (. <>) . toFilePath
+
+--------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
-
-{-|
--- | Compiler form of 'stripExif'
-stripExifCompiler :: Compiler (Item B.ByteString)
-stripExifCompiler = fmap stripExif <$> getResourceLBS
-
-
---------------------------------------------------------------------------------
--- | Strip exif data so you don't leak important information on the web.
-stripExif :: B.ByteString -> B.ByteString
-stripExif bs
-    | B.length bs < 2  = bs
-    | otherwise        = case word16be x of
-        0xffd8 -> toLazyByteString $ mconcat [lazyByteString x, parseAppDataSection xs]
-        _      -> bs
-  where (x, xs) = B.splitAt 2 bs
-
-parseAppDataSection :: B.ByteString -> Builder
-parseAppDataSection bs
-    | B.length bs < 4 = lazyByteString bs
-    | otherwise       = case marker' of
-        0xffe1  -> mconcat [stripExifFromApp1 pre, lazyByteString post] -- APP1 section (exif) - skip
-        0xffda  -> lazyByteString bs   -- End of App Data sections - return the rest
-        _       -> mconcat [lazyByteString pre, parseAppDataSection post] -- Write the rest
-  where (x, xs)   = B.splitAt 2 bs
-        (x', xs') = B.splitAt 2 xs
-        (pre, post) = B.splitAt (fromIntegral datasize') bs
-        marker = word16be x
-        datasize = word16be x'
-        marker' = trace ("marker is: " ++ show marker) marker
-        datasize' = trace ("datasize is: " ++ show datasize) datasize
-
-stripExifFromApp1 :: B.ByteString -> Builder
-stripExifFromApp1
-
-
--- from Data.Binary.Get
-word16be :: B.ByteString -> Word16
-word16be s = (fromIntegral (s `B.index` 0) `shiftl_w16` 8) .|.
-    fromIntegral (s `B.index` 1)
-
-shiftl_w16 :: Word16 -> Int -> Word16
-shiftl_w16 = shiftL
--}
